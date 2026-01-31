70b70973317b5434c9f0359f87162513e0cce2f1
dmitriiybondar <dmitrybondar6@gmail.com>
2026-01-31 14:12:58 +0200

    lesson 24.01

---
+++ Assets/Scripts/LevelGenerator.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelGenerator : MonoBehaviour
+{
+    [SerializeField] private GameObject groundPref, grassPref;
+
+    private int _baseHeight = 1,
+                _maxBlockCountY = 10,
+                _chunkSize = 16,
+                _perlinNoiseSensetivity = 25,
+                _chunkCount = 4;
+
+    private float seedX, seedY;
+
+    private void CreateChunk(int chunkNumX, int chunkNumZ)
+    {
+        GameObject chunk = new GameObject();
+        float chunkX = chunkNumX * _chunkSize + _chunkSize / 2;
+        float chunkZ = chunkNumZ * _chunkSize + _chunkSize / 2;
+        chunk.transform.position = new Vector3(chunkX, 0f, chunkZ);
+        chunk.name = "Chunk: " + chunkNumX + ", " + chunkNumZ;
+        chunk.AddComponent<MeshFilter>();
+        chunk.AddComponent<MeshRenderer>();
+        chunk.AddComponent<Chunk>();
+
+        for (int x = chunkNumX * _chunkSize; x < chunkNumX * _chunkSize + _chunkSize; x++)
+        {
+            for (int z = chunkNumZ * _chunkSize; z < chunkNumZ * _chunkSize + _chunkSize; z++)
+            {
+                float xSample = seedX + (float)x / _perlinNoiseSensetivity;
+                float ySample = seedY + (float)z / _perlinNoiseSensetivity;
+                float sample = Mathf.PerlinNoise(xSample, ySample);
+                int height = _baseHeight * (int)(sample * _maxBlockCountY);
+
+                for (int y = 0; y < height; y++)
+                {
+                    GameObject block = Instantiate(groundPref, new Vector3(x, y, z), Quaternion.identity);
+                    block.transform.SetParent(chunk.transform);
+                }
+            }
+        }
+    }
+    
+    void Start()
+    {
+        seedX = 30;
+        seedY = 71;
+        CreateChunk(0, 0);
+    }
+
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private const float gravity = 9.8f, speed = 5f, jumpForce = 8f, turnSpeed = 90f;
+    private float _verticalSpeed = 0f, _mouseX = 0f, _mouseY = 0f, _currentAngleX = 0f;
+
+    private CharacterController _controller;
+    [SerializeField] private Camera _camera;
+    
+    
+    void Start()
+    {
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
+        _controller = GetComponent<CharacterController>();
+    }
+
+    private void RotateCharacter()
+    {
+        _mouseX = Input.GetAxis("Mouse X");
+        _mouseY = Input.GetAxis("Mouse Y");
+        
+        transform.Rotate(new Vector3(0f, _mouseX * turnSpeed * Time.deltaTime, 0f));
+        _currentAngleX += _mouseY * turnSpeed * Time.deltaTime * -1f;
+        _currentAngleX = Mathf.Clamp(_currentAngleX, -60f, 60f);
+        
+        _camera.transform.localEulerAngles = new Vector3(_currentAngleX, 0f, 0f);
+    }
+
+    private void MoveCharacter()
+    {
+        Vector3 velocity = new Vector3(Input.GetAxis("Horizontal"), 0f, Input.GetAxis("Vertical"));
+        velocity = transform.TransformDirection(velocity) * speed;
+        
+        if (_controller.isGrounded)
+        {
+            _verticalSpeed = 0f;
+            if (Input.GetButton("Jump"))
+            {
+                _verticalSpeed = jumpForce;
+            }
+        }
+        
+        _verticalSpeed -= gravity * Time.deltaTime;
+        velocity.y = _verticalSpeed;
+        _controller.Move(velocity * Time.deltaTime);
+    }
+
+    void Update()
+    {
+        RotateCharacter();
+        MoveCharacter();
+    }
+}
