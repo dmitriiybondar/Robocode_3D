6b8477572bc6e79c6ceb6c9d186254525688ed11
dmitriiybondar <dmitrybondar6@gmail.com>
2026-01-31 17:56:45 +0200

    lesson 31.01

--- Assets/Scripts/Chunk.cs
+++ Assets/Scripts/Chunk.cs
@@ -4,15 +4,41 @@ using UnityEngine;
 
 public class Chunk : MonoBehaviour
 {
-    // Start is called before the first frame update
+    private const int VisibilityDistance = 30;
+    private bool _isVisible;
+    private Transform _playerT;
+    private Vector3 _chunkPos;
+    
     void Start()
     {
-        
+        _playerT = GameObject.Find("Capsule").transform;
+        _chunkPos = transform.position;
+        _isVisible = true;
     }
 
-    // Update is called once per frame
     void Update()
     {
+        float distance = Vector3.Distance(_chunkPos, new Vector3(_playerT.position.x, 
+                                                                0f, 
+                                                                _playerT.position.z));
+        if (distance > VisibilityDistance && _isVisible)
+        {
+            SetActivity(false);
+        } 
+        else if (distance < VisibilityDistance && !_isVisible)
+        {
+            SetActivity(true);
+        }
+    }
+
+    private void SetActivity(bool isActive)
+    {
+        int childrenCount = transform.childCount;
+        for (int i = 0; i < childrenCount; i++)
+        {
+            transform.GetChild(i).gameObject.SetActive(isActive);
+        }
         
+        _isVisible = isActive;
     }
 }
--- Assets/Scripts/LevelGenerator.cs
+++ Assets/Scripts/LevelGenerator.cs
@@ -36,7 +36,15 @@ public class LevelGenerator : MonoBehaviour
 
                 for (int y = 0; y < height; y++)
                 {
-                    GameObject block = Instantiate(groundPref, new Vector3(x, y, z), Quaternion.identity);
+                    GameObject block;
+                    if (y == height - 1)
+                    {
+                        block = Instantiate(grassPref, new Vector3(x, y, z), Quaternion.identity);
+                    }
+                    else
+                    {
+                        block = Instantiate(groundPref, new Vector3(x, y, z), Quaternion.identity);
+                    }
                     block.transform.SetParent(chunk.transform);
                 }
             }
@@ -45,9 +53,16 @@ public class LevelGenerator : MonoBehaviour
     
     void Start()
     {
-        seedX = 30;
-        seedY = 71;
-        CreateChunk(0, 0);
+        seedX = Random.Range(0, 10);
+        seedY = Random.Range(0, 10);
+        
+        for (int x = 0; x < _chunkCount; x++)
+        {
+            for (int z = 0; z < _chunkCount; z++)
+            {
+                CreateChunk(x, z);
+            }
+        }
     }
 
     void Update()
